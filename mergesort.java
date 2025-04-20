795fa27d1c3bd5de660045850c1931f638019c8a
chen-j3 <jchen753@binghamton.edu>
2025-04-20 19:38:51 -0400

    thoughts

---
+++ mergesort.java
@@ -0,0 +1,51 @@
+//distributed merge sort
+// Spark core classes
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+
+// Java utility classes
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Random;
+
+// Spark Pair class
+import scala.Tuple2;
+
+public static void main(String[] args) throws Exception{
+    // if (args.length < 1) {
+    //     System.err.println("Usage: DistributedMergeSort <file>");
+    //     System.exit(1);
+    // }
+
+    //initialize spark
+    SparkConf conf = new SparkConf().setAppName("DistributedMergeSort");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    //create random data
+    List<Integer> randomNumbers = newArrayList<>();
+    Random rand = new Random();
+    for (int i = 0; i < numItems; i++){
+        randomNumbers.add(rand.nextInt(1_000_000));
+    }
+    //RDD
+    JavaRDD<String> data = sc.parallelize(randomNumbers, numPartitions);
+
+    
+    //pass eahc element through a function func map(func) ?? maybe not
+        
+        //data partitionned and sorted by different nodes  map.Partitions????
+        //slpit data into chunks to be sorted in parallel
+            //how to split data
+        //data randomly created ? not sure
+
+    //merge results
+
+    //done
+    sc.stop();
+}
+
+
